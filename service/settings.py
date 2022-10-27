8d5d4e9fda19fbc76bf6758fc31fad1bca8a3379
Damian Stone <damianstonedev@gmail.com>
2022-10-27 17:53:44 +0100

    Fixing CRF problem

--- service/settings.py
+++ service/settings.py
@@ -30,7 +30,7 @@ SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
 # os environ come from the env variables of aws
 if "PRODUCTION" in os.environ:
-    DEBUG = False
+    DEBUG = True
     
     SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
     
