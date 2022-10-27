0bdb6cf748e5a2d5d512a5b0edfd5aad52354add
Damian Stone <damianstonedev@gmail.com>
2022-10-27 17:58:44 +0100

    session middleware out

--- service/settings.py
+++ service/settings.py
@@ -114,7 +114,7 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
+    # "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
