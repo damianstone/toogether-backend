c24d10c8dd24fff27ef5b7f60da7c3e0c85fdf79
Damian Stone <damianstonedev@gmail.com>
2022-10-27 22:56:20 +0100

    as view fix

--- service/settings.py
+++ service/settings.py
@@ -124,7 +124,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     # "django.middleware.csrf.CsrfViewMiddleware",
-    "service.core.middle",
+    "service.middle.DisableCSRFMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
