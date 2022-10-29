e8d3a7b519c43a3244f6fa93c78fd53287a598be
Damian Stone <damianstonedev@gmail.com>
2022-10-29 22:15:34 +0100

    GDAL path env

--- service/settings.py
+++ service/settings.py
@@ -30,6 +30,8 @@ SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 # os environ come from the env variables of aws
 if "PRODUCTION" in os.environ:
     
+    GDAL_LIBRARY_PATH = '/usr/local/lib/libgdal.dylib'
+    
     DEBUG = False
 
     SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
