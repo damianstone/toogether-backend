98ede4ba029eabc65c49f307cd6509c9630ce0aa
damianstone <damianstonedev@gmail.com>
2023-03-17 20:36:56 +0000

    Format from workflow

--- api/views/profile_views.py
+++ api/views/profile_views.py
@@ -254,16 +254,20 @@ class ProfileViewSet(ModelViewSet):
     def recovery_code(self, request):
         data = request.data
         # * 1 - comprobar que el mail existe en la base de datos usando el email
-        # * 2 - si no existe devolver una respuesta con un error 
-        # * 3 - si el mail existe enviar un codigo por mail para restablecer la contraseña 
-        # * 4 - el codigo debe expirar despues de 5 minutos 
-        Profile_email =  models.Profile.objects.filter(email=data["email"])
-        VerificationCode_email = models.VerificationCode.objects.filter(email=data["email"])
-        
+        # * 2 - si no existe devolver una respuesta con un error
+        # * 3 - si el mail existe enviar un codigo por mail para restablecer la contraseña
+        # * 4 - el codigo debe expirar despues de 5 minutos
+        Profile_email = models.Profile.objects.filter(email=data["email"])
+        VerificationCode_email = models.VerificationCode.objects.filter(
+            email=data["email"]
+        )
+
         if Profile_email.exists():
             letters_and_digits = string.ascii_letters + string.digits
-            codeGenerator = ''.join(random.choice(letters_and_digits.upper()) for i in range(6))
-            
+            codeGenerator = "".join(
+                random.choice(letters_and_digits.upper()) for i in range(6)
+            )
+
             if VerificationCode_email.exists():
                 current_user = models.VerificationCode.objects.get(email=data["email"])
                 current_user.code = codeGenerator
@@ -273,62 +277,84 @@ class ProfileViewSet(ModelViewSet):
                 new_user = models.VerificationCode.objects.create(
                     email=data["email"],
                     code=codeGenerator,
-                    expires_at=timezone.now() + timedelta(minutes=5))
+                    expires_at=timezone.now() + timedelta(minutes=5),
+                )
                 new_user.save()
 
             send_mail(
-            'Reset your password',
-            f'Here is your access code {codeGenerator}',
-            'toogethersite@gmail.com',
-            [data["email"]],
-            fail_silently=False,
+                "Reset your password",
+                f"Here is your access code {codeGenerator}",
+                "toogethersite@gmail.com",
+                [data["email"]],
+                fail_silently=False,
             )
-            return Response({"message": "Your recovery email was sent succesfully"}, status=status.HTTP_200_OK)
-        else: 
-            return Response({"message": "email doesn't exist"}, status=status.HTTP_400_BAD_REQUEST)
-    
+            return Response(
+                {"message": "Your recovery email was sent succesfully"},
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                {"message": "email doesn't exist"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
     @action(detail=False, methods=["post"], url_path=r"actions/validate-code")
     def validate_code(self, request):
-        
+
         data = request.data
 
-        VerificationCode_code =models.VerificationCode.objects.filter(code=data["code"])
+        VerificationCode_code = models.VerificationCode.objects.filter(
+            code=data["code"]
+        )
 
         if VerificationCode_code.exists():
             expiration = models.VerificationCode.objects.get(email=data["email"])
             user = models.Profile.objects.get(email=data["email"])
 
-            if(timezone.now() < expiration.expires_at):
+            if timezone.now() < expiration.expires_at:
                 serializer = serializers.ProfileSerializer(user, many=False)
-                return Response({"user": f'{user}', "VERIFIED": True, "AccessToken": serializer.data["token"]}, status=status.HTTP_200_OK)
+                return Response(
+                    {
+                        "user": f"{user}",
+                        "VERIFIED": True,
+                        "AccessToken": serializer.data["token"],
+                    },
+                    status=status.HTTP_200_OK,
+                )
             else:
-                    return Response({"message": "Expirated code"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"message": "Expirated code"}, status=status.HTTP_400_BAD_REQUEST
+                )
         else:
-            return Response({"message": "Invalid Code"}, status=status.HTTP_400_BAD_REQUEST) 
+            return Response(
+                {"message": "Invalid Code"}, status=status.HTTP_400_BAD_REQUEST
+            )
         # * 1 - input -> code and user_email
         # * 2 - comprobar que el codigo sea valido y que no este expirado
         # * 3 - si esta expirado o no es valido enviar diferentes respuestas (expirado / no valido)
         # * 5 - si el codigo no esta expirado y esta validado, traer el usuario usando el email
         # * 6 - autentificar al usario (devolver el auth token) y la propiedad VERIFIED: boolean
-    
+
     @action(detail=False, methods=["post"], url_path=r"actions/reset-password")
     def reset_password(self, request):
         # este endpoint necesita authentification (token)
-        current_profile = request.data 
+        current_profile = request.data
         profile_to_change = models.Profile.objects.get(email=current_profile["email"])
 
         if current_profile["new_pasword"] == current_profile["repeated_new_pasword"]:
             profile_to_change.password = make_password(current_profile["new_pasword"])
             profile_to_change.save()
             return Response({"OPERATION_SUCCESS": True}, status=status.HTTP_200_OK)
-        
+
         else:
-            return Response({"OPERATION_SUCCESS": False, "message": "passwords are not equals"}, status=status.HTTP_200_OK)
-        
+            return Response(
+                {"OPERATION_SUCCESS": False, "message": "passwords are not equals"},
+                status=status.HTTP_200_OK,
+            )
+
         # * 1 - input user_mail, new_passord y repeated_new_password
-        # * 2 - cambiar la contraseña del usuario y encriptarla 
+        # * 2 - cambiar la contraseña del usuario y encriptarla
         # * 3 - return OPERATION_SUCCESS: boolean
-        
+
         pass
 
 
--- service/settings.py
+++ service/settings.py
@@ -238,9 +238,9 @@ else:
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.googlemail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'toogethersite@gmail.com'
-EMAIL_HOST_PASSWORD = 'xdgjubgisgbtoewt'
+EMAIL_HOST_USER = "toogethersite@gmail.com"
+EMAIL_HOST_PASSWORD = "xdgjubgisgbtoewt"
 EMAIL_USE_TLS = True
