ef36fbab417efca471f8306135be9d20675c99dd
MartinOravecSvK <oravec.martin01@gmail.com>
2023-03-23 22:30:56 +0000

    package-lock after merge

--- service/settings.py
+++ service/settings.py
@@ -238,9 +238,9 @@ else:
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = os.environ["EMAIL_BACKEND"]
-EMAIL_HOST = os.environ["EMAIL_HOST"]
-EMAIL_PORT = os.environ["EMAIL_PORT"]
-EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
-EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
-EMAIL_USE_TLS = os.environ["EMAIL_USE_TLS"]
+# EMAIL_BACKEND = os.environ["EMAIL_BACKEND"]
+# EMAIL_HOST = os.environ["EMAIL_HOST"]
+# EMAIL_PORT = os.environ["EMAIL_PORT"]
+# EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+# EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+# EMAIL_USE_TLS = os.environ["EMAIL_USE_TLS"]
