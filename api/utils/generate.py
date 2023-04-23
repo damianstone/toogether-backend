c2f0f1c0c2de87a5ff50020645d7f518a7607ac7
damianstone <damianstonedev@gmail.com>
2023-04-23 18:31:58 +0000

    Format from workflow

--- api/utils/generate.py
+++ api/utils/generate.py
@@ -1,9 +1,10 @@
 import string
 import random
 
+
 def generate_group_code():
     alphabet = string.ascii_uppercase
-    letters = ''.join(random.choice(alphabet) for i in range(3))
-    digits = ''.join(random.choice(string.digits) for i in range(3))
+    letters = "".join(random.choice(alphabet) for i in range(3))
+    digits = "".join(random.choice(string.digits) for i in range(3))
     group_code = letters + digits
     return group_code
