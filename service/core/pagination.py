86304f946945011e39c7414ef9f3943087ee8cc7
Damian Stone <damianstonedev@gmail.com>
2023-05-13 02:57:04 +0100

    change pagination to 50

--- service/core/pagination.py
+++ service/core/pagination.py
@@ -17,8 +17,8 @@ class CustomNumberPagination(PageNumberPagination):
 
 
 class ChatPagination(PageNumberPagination):
-    page_size =  30
-    max_page_size = 30
+    page_size =  50
+    max_page_size = 50
     
     def get_paginated_response(self, data):
         return Response({
